259605670160c0295e2ec19322c03dfc57e2bc04
Naomi Carrigan <nhcarrigan@gmail.com>
2022-09-28 01:48:54 -0700

    feat: workflows

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ArtComponent } from './art/art.component';
+import { OutfitsComponent } from './outfits/outfits.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, ArtComponent, OutfitsComponent],
     }).compileComponents();
   });
 
@@ -20,12 +20,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('reference');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('reference app is running!');
   });
 });
