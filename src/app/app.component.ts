c9376afb25ffeafe3a65aad8c0b8723a343d8b51
Naomi Carrigan <nhcarrigan@gmail.com>
2022-10-28 00:05:41 -0700

    feat: overhaul ref sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reference';
-  view = '';
+  view = 'ref';
   setView(view: string) {
     this.view = view;
   }
