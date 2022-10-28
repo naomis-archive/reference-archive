41688ad5c2327f5593442e71b019226a90603de3
Naomi Carrigan <nhcarrigan@gmail.com>
2022-10-28 00:07:25 -0700

    fix: revert change in view for dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reference';
-  view = 'ref';
+  view = '';
   setView(view: string) {
     this.view = view;
   }
